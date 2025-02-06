10b2fbd4d8061bd3d8d91b48c7e241bb2ec4135f
kiasito <kastnmh@gmail.com>
2025-02-06 06:24:32 +0000

    djhgfj

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,9 @@ class EllipticCurveModPrimeNum():
             ) % self.p
 
             return (x3, y3)
+        
 win = st.number_input("素数", min_value=2)  
+st.write(1,0,0,1,win)
 weight = st.number_input("体重を入力してください", min_value=0)
 weighta = st.number_input("体重を入力してください", min_value=1)      
 ell = EllipticCurveModPrimeNum(1,0,0,1,win)
